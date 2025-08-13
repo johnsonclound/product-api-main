fed2acbfa11f84bd9c0abdf637665a4511ec64be
johnsonclound <66070242@kmitl.ac.th>
2025-08-13 13:25:00 +0700

    Fix typo in productController import and enhance products.ejs layout and styling

--- routes/webRoutes.js
+++ routes/webRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const db = require('../config/db');
-const productController = reqiure('../controllers/productController');
+const productController = require('../controllers/productController');
 
 router.get('/', productController.getProductsView);
 
